7e3d5aac37b37969d9b3c9f1f6efbee9bd6a8f8c
1b00 <1b00@pm.me>
2023-08-14 08:14:15 +0300

    task 3: add and increase MAX_INT_BITS for flag and value

--- tests/Task3.spec.ts
+++ tests/Task3.spec.ts
@@ -104,13 +104,13 @@ describe('Task3', () => {
         tb.writeNumber(5)
         tb.writeNumber(6)
         tb.writeCell(
-            (new Builder()).storeUint(0b1010001011, 10)
+            (new Builder()).storeUint(0b1010001010n, 10)
             .storeRef(
-                (new Builder()).storeUint(0b111111101, 9)
+                (new Builder()).storeUint(0b111111101n, 9)
                 .storeRef(
-                    (new Builder()).storeUint(0b11111110, 8)
+                    (new Builder()).storeUint(0b11111110n, 8)
                     .storeRef(
-                        (new Builder()).storeUint(0b111111101, 9)
+                        (new Builder()).storeUint(0b111111101n, 220)
                     )
                 )
             )
